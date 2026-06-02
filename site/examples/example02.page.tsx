dc445938160192c78749c2bb33694230a5f76b82
Sangit Manandhar <sangit@tscircuit.com>
2026-06-02 00:05:55 -0700

    fix

--- site/examples/example02.page.tsx
+++ site/examples/example02.page.tsx
@@ -1,16 +1,60 @@
 import { CellBoundariesDebugger } from "../fixture/CellBoundariesDebugger"
 
 const cells = [
-  { minX: 38, minY: 62, maxX: 150, maxY: 178 },
-  { minX: 230, minY: 28, maxX: 360, maxY: 138 },
-  { minX: 485, minY: 90, maxX: 612, maxY: 230 },
-  { minX: 105, minY: 255, maxX: 248, maxY: 370 },
-  { minX: 340, minY: 215, maxX: 430, maxY: 345 },
-  { minX: 655, minY: 260, maxX: 760, maxY: 385 },
-  { minX: 35, minY: 455, maxX: 185, maxY: 560 },
-  { minX: 280, minY: 500, maxX: 395, maxY: 640 },
-  { minX: 520, minY: 430, maxX: 675, maxY: 535 },
-  { minX: 760, minY: 515, maxX: 890, maxY: 650 },
+  {
+    minX: -13.4075,
+    minY: 3.0599999999999996,
+    maxX: -10.5925,
+    maxY: 12.940000000000001,
+  },
+  {
+    minX: -15.764999999999999,
+    minY: -3.25,
+    maxX: -4.535,
+    maxY: 0.42500000000000004,
+  },
+  {
+    minX: -1.4649999999999999,
+    minY: 6.15,
+    maxX: 1.4649999999999999,
+    maxY: 8.55,
+  },
+  {
+    minX: 8.85,
+    minY: 5.175000000000001,
+    maxX: 13,
+    maxY: 9.25,
+  },
+  {
+    minX: -2.2,
+    minY: -2.5944553499999996,
+    maxX: 3.75,
+    maxY: 1,
+  },
+  {
+    minX: 9.049999999999999,
+    minY: -0.4944553499999995,
+    maxX: 12.185,
+    maxY: 0.8944553499999994,
+  },
+  {
+    minX: -1.95,
+    minY: -8.9,
+    maxX: 1.1225,
+    maxY: -6.40554465,
+  },
+  {
+    minX: 10.5,
+    minY: -9.05,
+    maxX: 11.899999999999999,
+    maxY: -7.75,
+  },
+  {
+    minX: -13.2325,
+    minY: -10.4,
+    maxX: -8.1675,
+    maxY: -7.6,
+  },
 ]
 
 export default function CellBoundariesPipelineDemo() {
